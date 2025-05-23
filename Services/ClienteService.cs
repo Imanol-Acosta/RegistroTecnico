f12033a4bf0dceb234618fd9a0414141b6f9277c
Imanol Acosta Efres <yoloklk2002@gmail.com>
2025-05-23 01:17:19 -0400

    Agregando clienteService

---
+++ Services/ClienteService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using System.Linq.Expressions;
+
+
+namespace RegistroTecnico.Services;
+
+    public class ClienteService(IDbContextFactory<Contexto> dbFactory)
+    {
+        public async Task<bool> Guardar(Cliente cliente) 
+        {
+        if (!await Existe(cliente))
+            return await Insertar(Cliente);
+        else 
+            return await Modificar(cliente);
+        }
+
+    private async Task<bool> Existe(int clienteId)
+    {
+        await using var contexto = await dbFactory.CreateDbContextAsync();
+        return await contexto.Clientes.AnyAsync(c => c.ClienteId == clienteId);
+    }
+
+
+
+
+
+
+}
+
