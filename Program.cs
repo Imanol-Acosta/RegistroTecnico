06931a03c14d5931b1bbe782efe832ea14af8134
Imanol Acosta Efres <yoloklk2002@gmail.com>
2025-05-24 00:18:02 -0400

    Eliminando codigo innecesario

--- Program.cs
+++ Program.cs
@@ -3,49 +3,32 @@ using RegistroTecnico.DAL;
 using Microsoft.EntityFrameworkCore;
 using RegistroTecnico.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-
-builder.Services.AddRazorPages();
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
+// Conexión a base de datos
 var Constr = builder.Configuration.GetConnectionString("ConStr");
-
 builder.Services.AddDbContextFactory<Contexto>(options =>
     options.UseSqlServer(Constr));
 
-
-
-//Inyeccion del services
+// Inyección de servicios
 builder.Services.AddScoped<TecnicoService>();
 builder.Services.AddScoped<ClienteService>();
 
-
-
-
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
